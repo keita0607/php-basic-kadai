08360d1fceaeac88c430b52f77e5a974768b4e10
keita0607 <166913184+keita0607@users.noreply.github.com>
2024-07-15 09:39:18 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,48 @@
+<?php
+class Food {
+    private $name;
+    private $price;
+    
+    public function show_name() {
+        echo $this-> price . '<br>';
+    }
+
+    public function __construct(string $name, int $price){
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    }
+
+
+class Animal {
+    private $name;
+    private $height;
+    private $weight;
+
+    public function show_height() {
+        echo $this-> height . '<br>';
+    }
+
+    public function __construct(string $name, int $height, int $weight){
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+    }
+
+}
+
+$food = new Food('potato', 250);
+$animal = new Animal('dog', 60, 5000);
+
+print_r($food);
+echo '<br>';
+print_r($animal);
+echo '<br>';
+
+$food->show_name();
+$animal->show_height();
+
+
+?>
+
